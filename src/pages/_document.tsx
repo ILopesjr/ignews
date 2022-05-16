d2723871fb9c2ba462684d1ba566bf01c890deef
Ivanildo Lopes <ivanildo.lopes.jr@gmail.com>
2022-05-16 16:12:13 -0300

    adicionando component Head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
   render() {
@@ -6,11 +6,12 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com"  />
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&family=Spectral:wght@700&display=swap"
             rel="stylesheet"
           />
+          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
         </Head>
         <body>
           <Main />
